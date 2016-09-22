6661f148ecd52c9295b9b4c2f059f63c26e6bc1c
Meta Andrini Utari <meanut.peanut@gmail.com>
2016-09-22 15:41:23 +0700

    Added plot figures and made minor adjustments on driver scripts

--- tugas1/Driver1.m
+++ tugas1/Driver1.m
@@ -1,11 +1,11 @@
-i = 1:10;
-t = zeros(1,10);
-for n=1:10
-    sz = n * 100;
-    A = rand(sz);
-    b = rand(sz, 1);
-    f = @() SolveLU(A, b);
-    t(n) = timeit(f);
+n = 100:100:1000;
+t1 = zeros(1,10); t2 = zeros(1,10);
+for i=1:10
+    A = rand(n(i));
+    b = rand(n(i), 1);
+    f = @() SolveLU(A, b); g = @() SolveLUPivot(A, b);
+    t1(i) = timeit(f); t2(i) = timeit(g);
 end
 
-plot(i, t)
+% table(n.', t1.', 'VariableNames',{'n' 't1'})
+% table(n.', t2.', 'VariableNames',{'n' 't2'})
