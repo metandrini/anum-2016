3a209358742c48dee994caff2bddbcb45bdc2f6f
Meta Andrini Utari <meanut.peanut@gmail.com>
2016-10-04 16:44:22 +0700

    Committed Householder edits

--- Householder.m
+++ Householder.m
@@ -2,12 +2,16 @@ function [ H ] = Householder( A )
 %HOUSEHOLDER Summary of this function goes here
 %   Detailed explanation goes here
     [m, n] = size(A);
-    H = eye(m);
-    for i=1:n
-        v = A(:,i) + norm(A(i:m,i)) * eye(m,1);
+    I = eye(m)
+    %for i=1:n
+        v = A(:,1) + norm(A(:,1)) * I(:,1);
         if v ~= zeros(m, 1)
-            H = H - 2*(v*v')/(v'*v);
+            H = eye(m) - 2*(v*v')/(v'*v);
         end
-    end
+        v = A(:,2) + norm(A(:,2)) * I(:,2);
+        if v ~= zeros(m, 1)
+            H = eye(m) - 2*(v*v')/(v'*v);
+        end
+    %end
 end
 
