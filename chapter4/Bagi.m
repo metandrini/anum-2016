f7ed3989e668c0fb92caf2afb278d40be276d0a4
Meta Andrini Utari <meanut.peanut@gmail.com>
2016-11-09 22:52:17 +0700

    Committed early attempt at Newton division algorithm

--- chapter4/Bagi.m
+++ chapter4/Bagi.m
@@ -1,11 +1,12 @@
-function [ x_star, k ] = Akar( a, tol )
+function [ x_star, k ] = Bagi( d, tol )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 k = 0;
-x0 = a/2;
-while abs(x0^2 - a) > tol
-    x0 = x0/2 + a/(2*x0);
+x0 = 1;
+while abs(1/x0 - d) > tol
+    x0 = x0 + (x0 * (1 - d*x0));
     k = k + 1;
 end
 x_star = x0;
 end
+
