22485dc27b29ecf52a849886cfaa60cd9fbba808
Meta Andrini Utari <meanut.peanut@gmail.com>
2016-11-13 21:51:23 +0700

    Updated Bagi.m (2)

--- chapter4/Bagi.m
+++ chapter4/Bagi.m
@@ -3,21 +3,26 @@ function [ x_star, k ] = Bagi( divsor, tol )
 %   Detailed explanation goes here
 k = 0;
 m = 0;
-while (divsor < 0.5) || (divsor > 1)
-    if divsor > 1
-        divsor = bitsra(divsor,1);
-    elseif divsor < 0.5
-        divsor = bitsll(divsor,1);
+d = divsor;
+while (d < 0.5) || (d > 1)
+    if d > 1
+        d = bitsra(d,1);
+    elseif d < 0.5
+        d = bitsll(d,1);
     end
     m = m + 1;
 end
-x0 = 2.82353 - 1.88235 * divsor;
-x1 = 0;
-while abs(x1 - x0) > tol
-    x1 = x0 + (x0 * (1 - divsor*x0));
+x0 = 2.82353 - 1.88235 * d;
+x_star = bitsra(x0,m);
+while abs(x_star - 1/divsor) > tol
+    x1 = x0 + (x0 * (1 - d*x0));
+    if divsor > 1
+        x_star = bitsra(x1,m);
+    elseif divsor < 0.5
+        x_star = bitsll(x1,m);
+    end
     x0 = x1;
     k = k + 1;
 end
-x_star = bitsra(x1,m);
 end
 
